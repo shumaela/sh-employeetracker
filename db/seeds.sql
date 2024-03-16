548ca79494157e32cee0c411ac4739c5c9028ae0
shumaela101~ <shumu.laeeq@gmail.com>
2024-03-16 02:40:17 -0400

    in process of resolving seeds sourcing error

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,17 +5,19 @@ VALUES
 ('HR'),
 ('Asset Protection');
 
-INSERT INTO role(title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
 ('FS Developer', 120000, 1),
 ('Accountant', 95000, 2),
 ('HR Manager', 100000, 3),
-('Security', 65000, 4);
+('Security', 65000, 4),
+('Financial Analyst', 100000, 2);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Sarah', 'Turner', 1, 3),
+('Sarah', 'Kaif', 1, 3),
 ('Steve', 'Roberts', 2, 3),
 ('Ava', 'Marie', 3, NULL),
 ('Don', 'Jackman', 4, 3),
+('Adam', 'Stark', 2, 3);
